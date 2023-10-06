67198d4b19dd959c4dac4dac75b95d6e04233bb0
Cody Stepp <codystepp691@gmail.com>
2023-10-05 21:28:47 -0500

    added to README, finished JavaScript!

--- README.md
+++ README.md
@@ -1,1 +1,42 @@
 # generate-a-password
+
+Generating a Password!
+
+        In this project, I was tasked to use JavaScript to allow a webstie to generate a password. 
+    When you click the generate a password button, it will prompt you with the length of characters 
+    that you would like to have, and show a couple questions asking if you would like to add things like 
+    special characters and numbers. once you confirm all of the questions, it will then create a random password 
+    for you! The best part is you are able to make as many s you'd like. 
+
+Getting Started
+
+        To get this project started, there was a repository that I pulled from that gave me the HTML, CSS, and 
+    some of the starter code for the JavaScript file. 
+
+    -side note, I actually didn't see that you could pull the starter code from the start, so I made all of the
+    css and html myself at one point. You can see this in my "added html and css" push. Whoops! Later on I went back
+    and pulled the proper code. 
+
+Acceptance Criteria
+
+    GIVEN I need a new, secure password
+    WHEN I click the button to generate a password
+    THEN I am presented with a series of prompts for password criteria
+    WHEN prompted for password criteria
+    THEN I select which criteria to include in the password
+    WHEN prompted for the length of the password
+    THEN I choose a length of at least 8 characters and no more than 128 characters
+    WHEN asked for character types to include in the password
+    THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
+    WHEN I answer each prompt
+    THEN my input should be validated and at least one character type should be selected
+    WHEN all prompts are answered
+    THEN a password is generated that matches the selected criteria
+    WHEN the password is generated
+    THEN the password is either displayed in an alert or written to the page
+
+Acknowledgments
+    * cloud recording from coding bootcamp
+    * youtube videos help a lot!
+    * W3 schools
+    * I was given the starter code by UT Coding Bootcamp!
--- script.js
+++ script.js
@@ -1,1 +1,79 @@
-alert('Hello Cody.');
+// Assignment code here
+// Add all possible characters that we could use for the password
+var characterLength = 8;
+var choiceArr = [];
+//this is where we are going to add all of the characters that we could potentially use in our password.
+var numberArr = ['1','2','3','4','5','6','7','8','9','0'];
+var specialCharArr = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+', '=', '-'];
+var lowerCaseArr = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+var uppperCaseArr = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+
+
+
+var generateBtn = document.querySelector("#generate");
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+
+//when button is clicked, generate series of prompts: length of password least 8 most 128
+//ask for character types to be included,
+//input should be validated and at least on character type should be selected 
+// when all prompts are answered then a password is generated. 
+
+// Write password to the #password input
+function writePassword() {
+  var correctPrompts =  getPrompts(); 
+  var passwordText = document.querySelector("#password");
+
+  if(correctPrompts){
+  var newPassword = generatePassword();
+  
+
+  passwordText.value = newPassword;
+
+ }  else {
+      passwordText.value ="";
+ }
+
+}
+
+function generatePassword() {
+var password = "";
+for(var i = 0; i < characterLength; ++i) {
+  var randomIndex = Math.floor(Math.random() * choiceArr.length);
+  password = password + choiceArr[randomIndex];
+}
+  return password;
+}
+
+function getPrompts() {
+    choiceArr =[];
+    characterLength = parseInt(prompt("Between 8-128 characters, how long do you want your password to be?"));
+      // just making sure that the user types in a valid number here
+    if(isNaN(characterLength) || characterLength < 8 || characterLength > 128) {
+      alert("That is not a valid input, please try again");
+      return false; // this is where we get through our prompts 
+    }
+
+    if (confirm("Would you like us to put lowercase letters in your password? click *Ok*")) {
+      choiceArr = choiceArr.concat(lowerCaseArr);
+
+    }
+
+    if (confirm("Would you like us to put Uppercase letters in your password? click *Ok*")) {
+      choiceArr = choiceArr.concat(uppperCaseArr);
+
+    }
+
+    if(confirm("Would you like us to use Special characters in your password? click *Ok*")){
+      choiceArr = choiceArr.concat(specialCharArr);
+    }
+
+    if (confirm("Would you like us to use numbers in your password? Click *Ok* ")) {
+      choiceArr = choiceArr.concat(numberArr);
+    }
+
+    return true;
+  
+}
